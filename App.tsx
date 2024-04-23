3c6b2923852945abdab85ffc88835369b51c91ad
harukymmm <comfortble-red@i.softbank.jp>
2024-04-23 17:12:27 +0900

    あ

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>情報システム論実習breadHunter</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
