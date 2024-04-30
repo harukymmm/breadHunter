1635813728c9c37b554d669a73e3256923456ec5
yorolleyhoo <obliviate219@gmail.com>
2024-04-30 17:20:55 +0900

    アイコンの画像をアップロード、画像を表示

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>情報システム論実習breadHunter</Text>
+      <Image source={require('./assets/App icon.png')} style={{width: 320, height: 320}} />
     </View>
   );
 }
