a9ef18dd939a01b0cb2c23508a3cbc1b180fd726
harukymmm <comfortble-red@i.softbank.jp>
2024-05-01 20:42:50 +0900

    initialScreen

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Screen1">
+      <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={StartScreen} options={{ headerShown: false }} />
         <Stack.Screen name="QuizSelect" component={QuizSelectScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Map" component={MapScreen} options={{ headerShown: false }} />
