08e2f4282cf41be3926df41dce236aa4de800f22
harukymmm <comfortble-red@i.softbank.jp>
2024-05-01 15:16:16 +0900

    icon周り修正

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>情報システム論実習breadHunter</Text>
-      <Image source={require('./assets/App icon.png')} style={{width: 320, height: 320}} />
+      <Image source={require('./assets/breadicon.png')} style={{width: 320, height: 320}} />
       <Button title="Start" onPress={() => console.log('Start')} />
     </View>
   );
