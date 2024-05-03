4e3a124066565cd556a6c8be5c71b0ab8ac2b765
yorolleyhoo <obliviate219@gmail.com>
2024-05-04 00:10:55 +0900

    フォントの追加、プラグインとしてexpo-fontの導入、QuizSelectScreenにフォントの適用（実装未確認）

--- screen/QuizSelectScreen/QuizSelectScreen.tsx
+++ screen/QuizSelectScreen/QuizSelectScreen.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { useFonts } from 'expo-font';
+
 
 export default function QuizSelectScreen() {
+  const [fontsLoaded, fontError] = useFonts({
+    'SmileySans-Oblique': require('./assets/fonts/SmileySans-Oblique.otf'),
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>QuizSelectScreenです</Text>
+      <Text style={{ fontFamily: 'SmileySans-Oblique', fontSize: 30}}>QuizSelectScreenです</Text>
+      <Text style={{ fontSize: 30 }}>Platform Default</Text>
       <Button title="Start" onPress={() => console.log('Start')} />
     </View>
   );
