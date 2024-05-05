e10e54972ed22f9736bfded98f64fbe3f9ce66d5
Tanned-podcast <kftwitter2020@gmail.com>
2024-05-05 19:21:39 +0900

    screenがあるようにclone

--- App.tsx
+++ App.tsx
@@ -18,3 +18,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello There! this is pull request!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
