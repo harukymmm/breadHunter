9e9c45287dd2d917232af174545486c232155688
harukymmm <comfortble-red@i.softbank.jp>
2024-05-07 17:00:37 +0900

    書き方修正

--- components/CustomSelectComponent.tsx
+++ components/CustomSelectComponent.tsx
@@ -18,31 +18,9 @@ const SelectFigComp = ({onPress, lunk, source}: Props) => {
         return null;
       }
     
-     const styles = StyleSheet.create({
-        lunkText: {
-            fontFamily: 'SmileySans-Oblique',
-            fontSize: 25,
-            color: '#332E21',
-            textAlign: 'center',
-        },
-        container: {
-            flex: 1,
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor:'#FBF7EF', //背景色
-            borderColor: '#FBF7EF', // 白色の背景枠
-            borderWidth: 5, // 枠の太さ
-              },
-        space: {
-            flex: 0,
-            height: 5
-              }
-        });
-
     return (
       <View style={styles.container}>
-      <Text style= {styles.lunkText}>{lunk}</Text>
+        <Text style= {styles.lunkText}>{lunk}</Text>
         <TouchableOpacity onPress={onPress}>
             <Image 
             source={source}  //画像のソースをpropsから受け取る
@@ -50,24 +28,45 @@ const SelectFigComp = ({onPress, lunk, source}: Props) => {
             />
         </TouchableOpacity>
         <View style={styles.space} />{/* 空白 */}
-      <ButtonCustom
-      borderColor="#FF8628"
-      borderWidth={5}
-      color="#FBF7EF"
-      height={35}
-      onClick={() => console.log("You clicked on the miniButton!")}
-      radius={90}
-      width={35}
-      children="" 
-      fontColor=''
-      fontSize={0}
-      justifyContent='center'
-      alignItems='center'
-      />
-      
+        <ButtonCustom
+          borderColor="#FF8628"
+          borderWidth={5}
+          color="#FBF7EF"
+          height={35}
+          onClick={() => console.log("You clicked on the miniButton!")}
+          radius={90}
+          width={35}
+          children="" 
+          fontColor=''
+          fontSize={0}
+          justifyContent='center'
+          alignItems='center'
+        />
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+    lunkText: {
+        fontFamily: 'SmileySans-Oblique',
+        fontSize: 25,
+        color: '#332E21',
+        textAlign: 'center',
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor:'#FBF7EF', //背景色
+        borderColor: '#FBF7EF', // 白色の背景枠
+        borderWidth: 5, // 枠の太さ
+          },
+    space: {
+        flex: 0,
+        height: 5
+          }
+    });
 
   
   export default SelectFigComp;
