fa9bf8d51f9835123a74ef1c6d7094fd44d9cd4f
harukymmm <comfortble-red@i.softbank.jp>
2024-05-14 16:31:20 +0900

    route統合

--- route.d.ts
+++ route.d.ts
@@ -1,10 +1,13 @@
 export type StackParamList = {
   Start: undefined;
   QuizSelect: undefined;
+  QuizDetail: { breadId: number };
   MapDefault: { breadId: number | null };
-  NearBakery: undefined;
-  TakePhotoF: undefined;
-  ResultCorrect: undefined;
-  ResultFalse: undefined;
-  ResultGiveUp: undefined;
+  NearBakery: { breadId: number | null };
+  TakePhotoF: { breadId: number | null };
+  BreadDetail: { breadId: number };
+  TakePhotoL: { breadId: number };
+  ResultCorrect: { breadId: number | null };
+  ResultFalse: { breadId: number | null };
+  ResultGiveUp: { breadId: number | null };
 };
--- screen/QuizSelectScreen/routeQuizSelect.d.ts
+++
@@ -1,4 +0,0 @@
-export type QuizParamList = {
-    QuizSelect: undefined;
-    QuizDetail: { breadId: number };
-  };
